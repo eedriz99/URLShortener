33504ed87c4c01f539601f381dd8b063e79b47a9
eedriz99 <akinsolaidris1999@gmail.com>
2024-02-28 20:17:33 +0100

    settings change

--- urlShortener/settings.py
+++ urlShortener/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
