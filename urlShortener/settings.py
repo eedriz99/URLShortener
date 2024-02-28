d54d2f3f09f402e912810b6675e3a3ba31809ad8
eedriz99 <akinsolaidris1999@gmail.com>
2024-02-28 20:55:08 +0100

    yaml change

--- urlShortener/settings.py
+++ urlShortener/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     # }
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgres://shortly_y73u_user:g1OOpJuukFT4rWUtk4rX0Qi8gL9LSLxS@dpg-cnfotleg1b2c73bb079g-a/shortly_y73u',
         conn_max_age=600
     )
 }
